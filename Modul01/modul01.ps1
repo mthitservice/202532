c70973ebb6904e635ee25f60059492d95b34dbde
Michael Lindner <Michael.Lindner@mth-it-service.com>
2025-06-03 12:03:07 +0200

    Stand Dienstag vor dem Mittag

---
+++ Modul01/modul01.ps1
@@ -0,0 +1,18 @@
+# Verwenden der Hilfe
+
+Get-Help about*
+
+Get-Help about_Aliases
+
+Get-Help about_Eventlogs -ShowWindow
+
+Get-Help about_TestDrive -ShowWindow
+
+Get-Help *beep*
+
+# Aliase
+# dir -> Get-ChildItem
+dir
+Get-ChildItem
+# Welcher Befehl befindet sich hinter einem Alias
+Get-Alias dir
